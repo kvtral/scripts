2556022328ba3bbaf5a587d75d34e73e3c1bd38f
Alvaro J. Carrillanca <65778446+kvtral@users.noreply.github.com>
2022-08-22 17:39:14 -0400

    utilizar colores en bash
    Utilización de colores en bash

---
+++ colores.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CYAN="\033[01;36m"
+NONE="\033[00m"
+YELLOW="\x1b[33m"
+BLUE="\x1b[34m"
+RED="\x1b[31m"
+GREEN="\x1b[32m"
+PURPLE='\033[01;35m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+echo -e "${GREEN}------------------------------------------------------------------------------${NONE}" 
+echo -e "${CYAN}Utilizar colores en la consola con un script${NONE}"
+echo -e "${BLUE}###########${WHITE}################${RED}###############${NONE}" 
